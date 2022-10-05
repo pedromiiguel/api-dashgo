11e76ef62ece4aa2e3f203e0e0e4c049cd99bba1
Pedro <pedro.miguel@pixter.com.br>
2022-10-05 16:15:30 -0300

    added userID in request

--- src/middlewares/ensureAuthenticate.ts
+++ src/middlewares/ensureAuthenticate.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
+interface Ipaylod {
+  sub: string;
+}
+
 export function ensureAuthenticated(
   request: Request,
   response: Response,
@@ -15,7 +19,12 @@ export function ensureAuthenticated(
   const [, token] = authToken.split(' ');
 
   try {
-    verify(token, '7c18ae8f-6695-4add-87e6-89d6d2da059f');
+    const { sub } = verify(
+      token,
+      '7c18ae8f-6695-4add-87e6-89d6d2da059f'
+    ) as Ipaylod;
+
+    request.userId = sub;
 
     return next();
   } catch (error) {
